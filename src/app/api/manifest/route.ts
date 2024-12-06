49d348b165b1117fe01188821b3b65e9652032db
Kielinuppu <info@kielinuppu.fi>
2024-12-06 12:38:16 +0200

    manifest päivitys

--- src/app/api/manifest/route.ts
+++ src/app/api/manifest/route.ts
@@ -29,13 +29,13 @@ export async function GET() {
         ],
         "icons": [
     {
-      "src": "public/icons/icon-192x192.png",
+      "src": "/icons/icon-192x192.png",
       "sizes": "192x192",
       "type": "image/png",
       "purpose": "any maskable"
     },
     {
-      "src": "public/icons/icon-512x512.png",
+      "src": "/icons/icon-512x512.png",
       "sizes": "512x512",
       "type": "image/png",
       "purpose": "any maskable"
